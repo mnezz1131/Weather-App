fc377c3d1d5b6cce61ca6eed13e68b2a9dc762cb
Mike <mnezz1131@gmail.com>
2021-10-06 13:24:02 -0500

    code and debugging nearly done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Creating the fetch request for getting data from OpenWeather API 
 const fetchWeather = (dataInput) => {
   const ApiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${dataInput}&appid=b0ae55b6c429d2b3beee108ecdbd660e`
   console.log("making our request")
@@ -16,10 +17,10 @@ const fetchWeather = (dataInput) => {
       console.error(`ERROR: ${err}`)
     });
 }
-
+//creating the HTML elements and rendering them on the page
 const renderList = (resJSON) => {
   let searchResults = resJSON;
-  console.log(searchResults)
+  // console.log(searchResults)
   //Selecting location class adding Location and country
   const locationDiv = document.querySelector(".location");
   //empties the div for next search
@@ -30,16 +31,12 @@ const renderList = (resJSON) => {
   nameTag.style.backgroundColor = "gold"
   nameTag.style.borderBlockStyle = "solid"
   nameTag.style.fontSize = "20px"
-  const clouds = document.createElement("p")
-  clouds.innerText = `Clouds: ${searchResults.weather[0].description}`
-  const otherTag = document.createElement("p")
-  otherTag.innerText = `Humidity: ${searchResults.main.humidity}%, Pressure: ${searchResults.main.pressure}`
+locationDiv.appendChild(nameTag)
+  
 
-  locationDiv.append(nameTag)
-  locationDiv.appendChild(clouds)
-  locationDiv.appendChild(otherTag)
+ 
 
-  //#################################################################################
+  //################ Array and For Loop for Temps #################################################################
   const tempTag = document.createElement('UL');
   locationDiv.appendChild(tempTag)
 
@@ -67,11 +64,18 @@ const renderList = (resJSON) => {
       } else if (i === 3) {
         tempElem.innerText = `Temp High: ${faren},  ${celsius},  ${kelvin}`
       }
-      // tempElem.innerText = `${faren}, ${celsius}, ${kelvin}`
       tempTag.appendChild(tempElem)
     }
   }
   tempConvert()
+  const clouds = document.createElement("p")
+  clouds.innerText = `Clouds: ${searchResults.weather[0].description}`
+  locationDiv.appendChild(clouds)
+
+  const otherTag = document.createElement("p")
+  otherTag.innerText = `Humidity: ${searchResults.main.humidity}%, `
+  locationDiv.appendChild(otherTag)
+
   const windTag = document.createElement('p');
   locationDiv.appendChild(windTag)
   windTag.innerText = `Wind Speed: ${Math.round(searchResults.wind.speed*2.23694)} mph`
@@ -80,20 +84,19 @@ const renderList = (resJSON) => {
 
 
 // !################################################ WEEKLY FORECAST SECTION ################################################################################
-
-
+//Getting the City Longitude and Latitude from first fetch to  get the Extended Forecast 
 const getCoord = (resJSON) => {
   let lat = resJSON.coord.lat
   let lon = resJSON.coord.lon
   lat = Number(lat.toFixed(2).value)
   lon = Number(lon.toFixed(2).value)
 
-  console.log(typeof lat, lat, typeof lon, lon)
+  // console.log(typeof lat, lat, typeof lon, lon)
 
   const fetchForecast = (lat, lon) => {
 
     const forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=59.91&lon=10.75&units=imperial&exclude=minutely.hourly,current&appid=b0ae55b6c429d2b3beee108ecdbd660e`
-    console.log("making our request")
+    // console.log("making our request")
 
     fetch(forecastUrl)
       .then((res) => {
@@ -107,16 +110,21 @@ const getCoord = (resJSON) => {
       .catch((err) => {
         console.error(`ERROR: ${err}`)
       });
+    }
+    fetchForecast();
   }
-  fetchForecast();
-}
+  //Rendering the Extended Daily forecast
+  const renderDaily = (resJSON) => {
+    
+  const foreCastDiv = document.querySelector(".forecast");
+    // todo foreCastDiv.innerHTML = "";
 
-const renderDaily = (resJSON) => {
   const extendP = document.createElement("p")
   extendP.innerText = `Extended ForeCast Yo`
   document.querySelector(".forecast").appendChild(extendP)
+  
+  //Setting search results to a varialbe 
   const dailySearch = resJSON.daily
-  console.log(dailySearch[7].dt)
 
   // const unixTime = dailySearch[7].dt
   // const date = new Date(unixTime*1000);
@@ -126,17 +134,6 @@ const renderDaily = (resJSON) => {
   for (i = 0; i < dailySearch.length; i++) {
     console.log(dailySearch[i])
 
-    console.log(new Date(dailySearch[i].dt * 1000).toLocaleDateString("en-us"))
-    console.log(dailySearch[i].temp.day)
-    console.log(dailySearch[i].feels_like.day)
-    console.log(dailySearch[i].temp.min)
-    console.log(dailySearch[i].temp.max)
-    console.log(dailySearch[i].clouds)
-    console.log(dailySearch[i].dew_point)
-    console.log(dailySearch[i].humidity)
-    console.log(dailySearch[i].uvi)
-    console.log(dailySearch[i].weather[0].main)
-    console.log(dailySearch[i].weather[0].description)
     const day = new Date(dailySearch[i].dt * 1000).toLocaleDateString("en-us")
     const temp = dailySearch[i].temp.day
     const feels = dailySearch[i].feels_like.day
@@ -147,25 +144,44 @@ const renderDaily = (resJSON) => {
     const humid = dailySearch[i].humidity
     const main = dailySearch[i].weather[0].main
     const desc = dailySearch[i].weather[0].description
+    const icon = dailySearch[i].weather[0].icon
+    const uvi = dailySearch[i].uvi
+   
+    if (i !== 0) {
+      // ?Creating a DIV for each day in the weekly forecast
+
+      const newForeCastDiv = document.createElement("DIV")
+
+      newForeCastDiv.classList.add("newForeCastDiv")
+
+      foreCastDiv.appendChild(newForeCastDiv)
+
+      const foreP = document.createElement("p")
+      foreP.innerText = `${day}`
+      newForeCastDiv.appendChild(foreP)
+
+      const forecastUL = document.createElement('UL')
+      forecastUL.classList.add("foreCastUl")
+      newForeCastDiv.appendChild(forecastUL)
+      const foreCastElem = document.createElement("li")
+      foreCastElem.innerText = `Temp is: ${temp},    Feels Like: ${feels},    Min: ${min},   Max: ${max} `
+      forecastUL.appendChild(foreCastElem)
+
+      const forecastUL2 = document.createElement('UL')
+
+      newForeCastDiv.appendChild(forecastUL2)
+      const foreCastElem2 = document.createElement("li")
+      foreCastElem2.innerText = `Outlook: ${main} - ${desc}  ${icon}`
+      forecastUL2.appendChild(foreCastElem2)
 
 
-    // ?Creating a DIV for each day in the weekly forecast
-    const foreCastDiv = document.querySelector(".forecast");
-    const newForeCastDiv = document.createElement("DIV")
-    
-    newForeCastDiv.classList.add("newForeCastDiv")
-    foreCastDiv.appendChild(newForeCastDiv)
+      const forecastUL3 = document.createElement('UL')
 
-    const foreP = document.createElement("p")
-    foreP.innerText = `${day}`
-    newForeCastDiv.appendChild(foreP)
-
-    const forecastUL = document.createElement('UL')
-    forecastUL.classList.add("foreCastUl")
-    newForeCastDiv.appendChild(forecastUL)
-    const foreCastElem = document.createElement("li")
-    foreCastElem.innerText = `Temp is: ${temp},    Feels Like: ${feels},    Min: ${min},   Max: ${max} `
-    forecastUL.appendChild(foreCastElem)
+      newForeCastDiv.appendChild(forecastUL3)
+      const foreCastElem3 = document.createElement("li")
+      foreCastElem3.innerText = `Dewpoint: ${dew} - Humidity: ${humid} UVI: ${uvi}`
+      forecastUL3.appendChild(foreCastElem3)
+    }
 
   }
 
